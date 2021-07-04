1b43bd219d9a632802f264f977c00e610bde30ab
cosmos <454924039@qq.com>
2021-07-04 10:45:48 +0800

    >>>[msg]feat(jira-xxx【开发中】): {新增flyway测试用配置类，提交保存}

---
+++ src/main/java/org/example/todo/config/TestConfig.java
@@ -0,0 +1,15 @@
+package org.example.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+//    @Bean
+//    public FlywayMigrationStrategy clean() {
+//        return flyway -> {
+//            flyway.clean();
+//            flyway.repair();
+////            flyway.migrate();
+//        };
+//    }
+}
